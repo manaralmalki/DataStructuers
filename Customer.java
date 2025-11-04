3576b120d478279bba35a6c6e3c30b63f8fca2fb
manaralmalki <117475144+manaralmalki@users.noreply.github.com>
2025-11-04 16:41:52 +0300

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,97 @@
+package projectDs;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Customer {
+
+    private int customerId;
+    private String name;
+    private String email;
+    private LinkedList<Order> orders;
+    private LinkedList<Review> reviews;
+
+    
+
+    public Customer(int id, String name, String email) {
+        this.customerId = id;
+        this.name = name;
+        this.email = email;
+        this.orders = new LinkedList<>();
+        this.reviews=new LinkedList<>();
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void addOrder(Order o) {
+        orders.addLast(o);
+    }
+    
+    public void addReview(Review r) {
+    	reviews.addLast(r);
+    }
+
+    public void display() {
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("-------------------");
+        displayOrders();
+        displayReviews();
+        
+    }
+    public void displayReviews() {
+        System.out.println("Reviews for " + name + ":");
+
+        if (reviews.empty()) {
+            System.out.println("No Reviews");
+        } else {
+            reviews.findFirst();
+            while (true) {
+                reviews.retrieve().display();
+
+                if (reviews.last())
+                    break;
+
+                reviews.findNext();
+            }
+        }
+    }
+
+
+    public void displayOrders() {
+        if (orders.empty()) {
+            System.out.println("No orders for customer " + name);
+            return;
+        }
+
+        System.out.println("Orders for " + name + ":");
+        orders.findFirst();
+
+        while (true) {
+            orders.retrieve().display();
+
+            if (orders.last())
+                break;
+
+            orders.findNext();
+        }
+    }}
+    
+    
+    
+    
+    
+
+ 
+
